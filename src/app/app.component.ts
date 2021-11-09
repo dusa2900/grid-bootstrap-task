a8e746963af434c44e6f6d2d3aeccf3041d8e04a
dusa2900 <62232012+dusa2900@users.noreply.github.com>
2021-11-08 20:41:33 -0800

    custom-pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task';
+	fuels={
+	"petrolRate":102.35,
+	"dieselRate":100.10
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CarouselModule} from 'primeng/carousel';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import { CurrencyPipe } from './Pipe/currency.pipe';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyPipe,
+    GridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,CarouselModule,ButtonModule,ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
